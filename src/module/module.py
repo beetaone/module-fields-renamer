9205f9c35ee06f5cdf14e8692779bf0348ac7cb7
kubagrzelak <jakubgrzelak1@gmail.com>
2022-11-09 16:55:15 +0000

    Applied PR feedback

--- src/module/module.py
+++ src/module/module.py
@@ -18,6 +18,18 @@ if os.getenv("RENAME_FIELDS"):
         old_name, new_name = mapping.split('=')
         names_mapping[old_name] = new_name
 
+def renaming(data):
+    renamed_data = {}
+    for k, v in data.items():
+        # rename keys without changing the order
+        if k in names_mapping.keys():
+            renamed_data[names_mapping[k]] = v
+        else:
+            renamed_data[k] = v
+
+    return renamed_data
+
+
 def module_main(received_data: any) -> [any, str]:
     """
     Process received data by implementing module's main logic.
@@ -39,24 +51,10 @@ def module_main(received_data: any) -> [any, str]:
             processed_data = []
 
             for data in received_data:
-                sub_data = {}
-                for k, v in data.items():
-                    # rename keys without changing the order
-                    if k in names_mapping.keys():
-                        sub_data[names_mapping[k]] = v
-                    else:
-                        sub_data[k] = v
-                processed_data.append(sub_data)
+                processed_data.append(renaming(data))
 
         else:
-            processed_data = {}
-
-            for k, v in received_data.items():
-                # rename keys without changing the order
-                if k in names_mapping.keys():
-                    processed_data[names_mapping[k]] = v
-                else:
-                    processed_data[k] = v
+            processed_data = renaming(received_data)
 
         return processed_data, None
 
